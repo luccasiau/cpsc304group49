bdd25f2cf50b561b2abd4c95e8b4edf362605ca8
Lucca Siaudzionis <luccasiau@gmail.com>
2019-11-20 18:07:47 -0800

    Defining all tables

--- resources/createTables.sql
+++ resources/createTables.sql
@@ -1,3 +1,10 @@
+DROP TABLE customer;
+DROP TABLE reservation;
+DROP TABLE vehicle;
+DROP TABLE vehicletype;
+DROP TABLE rent;
+DROP TABLE return;
+
 CREATE TABLE customer (
   dlicense    VARCHAR(10) NOT NULL,
   name        VARCHAR(255) NOT NULL,
@@ -6,13 +13,66 @@ CREATE TABLE customer (
   PRIMARY KEY(dlicense)
 );
 
+CREATE TABLE vehicletype (
+    vtname          VARCHAR(20)   NOT NULL,
+    features        VARCHAR(255),
+    weeklyRate      FLOAT,
+    dailyRate       FLOAT,
+    hourlyRate      FLOAT,
+    kiloRate        FLOAT,
+    weeklyInsRate   FLOAT,
+    dailyInsRate    FLOAT,
+    hourlyInsRate   FLOAT,
+    PRIMARY KEY (vtname)
+);
+
+CREATE TABLE vehicle (
+    vlicense        VARCHAR(10) NOT NULL,
+    vtname          VARCHAR(20) NOT NULL,
+    odometer        INT,
+    status          CHAR(1),
+    colour          VARCHAR(20),
+    location        VARCHAR(255) NOT NULL,
+    city            VARCHAR(255) NOT NULL,
+    FOREIGN KEY (vtname) REFERENCES vehicletype,
+    PRIMARY KEY (vlicense)
+);
+
 CREATE TABLE reservation (
-  confno      VARCHAR(20)  NOT NULL,
-  dlicense    VARCHAR(20)  NOT NULL,
-  vtname      VARCHAR(20)  NOT NULL,
-  location    VARCHAR(255) NOT NULL,
-  city        VARCHAR(255) NOT NULL,
-  startdate   DATE         NOT NULL,
-  enddate     DATE         NOT NULL,
-  PRIMARY KEY(confno)
+    confno      VARCHAR(20)  NOT NULL,
+    dlicense    VARCHAR(20)  NOT NULL,
+    vtname      VARCHAR(20)  NOT NULL,
+    location    VARCHAR(255) NOT NULL,
+    city        VARCHAR(255) NOT NULL,
+    startdate   DATE         NOT NULL,
+    enddate     DATE         NOT NULL,
+    FOREIGN KEY (vtname) REFERENCES vehicletype,
+    FOREIGN KEY (dlicense) REFERENCES customer,
+    PRIMARY KEY(confno)
+);
+
+CREATE TABLE rent (
+    rentid      VARCHAR(20)   NOT NULL,
+    vlicense    VARCHAR(10)   NOT NULL,
+    dlicense    VARCHAR(10)   NOT NULL,
+    confno      VARCHAR(20),
+    fromDate    DATE          NOT NULL,
+    toDate      DATE          NOT NULL,
+    cardname    VARCHAR(255)  NOT NULL,
+    cardno      VARCHAR(16)   NOT NULL,
+    expdate     INT           NOT NULL,
+    FOREIGN KEY vlicense REFERENCES vehicle,
+    FOREIGN KEY dlicense REFERENCES customer,
+    FOREIGN KEY confno   REFERENCES reservation,
+    PRIMARY KEY (rentid)
+);
+
+CREATE TABLE return(
+    rentid      VARCHAR(20)     NOT NULL,
+    returnDate  DATE            NOT NULL,
+    odomoter    INT             NOT NULL,
+    fullTalk    BOOLEAN         NOT NULL,
+    revenue     FLOAT           NOT NULL,
+    FOREIGN KEY rentid  REFERENCES rent,
+    PRIMARY KEY (rentid)
 );
